0ed04e032342b004d8e39073325963ea50c8c1f5
mcondewol <mconde@wol.group>
2022-06-06 00:06:51 -0600

    recaptcha comment fixed

--- recaptcha.php
+++ recaptcha.php
@@ -11,7 +11,7 @@ function verificarToken($token, $claveSecreta)
         "http" => array(
             "header" => "Content-type: application/x-www-form-urlencoded\r\n",
             "method" => "POST",
-            "content" => http_build_query($datos), # Agregar el contenido definido antes
+            "content" => http_build_query($datos),
         ),
     );
     $contexto = stream_context_create($opciones);
